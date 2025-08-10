37cd3a9ea8fc3f5dde987f84287ca41be0607e09
KwachOjunga <ojungakwach@gmail.com>
2025-08-10 19:32:23 +0300

    add stub files to work as tests that will drive the API design of pyburn

--- src/tests/nn_tests.rs
+++ src/tests/nn_tests.rs
@@ -2,9 +2,11 @@
 
 #[cfg(test)]
 mod nn_test {
+    use burn::backend::NdArray;
     use dhat;
     use crate::tensor::wgpu_base;
     use crate::tensor::ndarray_base;
+    
     
     #[global_allocator]
     static ALLOC : dhat::Alloc = dhat::Alloc;
@@ -13,8 +15,12 @@ mod nn_test {
     fn test_tensor_memory_allocations(){
         let _profiler = dhat::Profiler::builder().testing().build();
 
-        // let nd_tensor = ndarray_base::TensorPy::new();
+        //  let nd_tensor = ndarray_base::Tensor1{
+        //     inner: burn::prelude::Tensor::<NdArray,1>::from_floats([1.0], &crate::nn::NDARRAYDEVICE)
+        //  };
+        let tensor:burn::prelude::Tensor<burn::backend::NdArray, 1> = burn::prelude::Tensor::<NdArray,1>::from_floats([1.0], &crate::nn::NDARRAYDEVICE);
         let stats = dhat::HeapStats::get();
+        // dhat::assert_eq!(nd_tensor.inner, tensor);
         println!("Number of blocks {:#?}", stats.curr_blocks);
         println!("Number of bytes {:#?}", stats.curr_bytes);
     }
